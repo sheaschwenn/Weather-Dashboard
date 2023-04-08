f7a95a2fc1ec1b15fbe4016dd3a53e9fff639818
Shea Schwenn <sheaschwenn@gmail.com>
2023-04-08 15:21:41 -0700

    text has a better format

--- assets/script.js
+++ assets/script.js
@@ -61,11 +61,11 @@ function getApi(event) {
 
 
     var city = searchInput.value
-    
+
 
     var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial&appid=" + apiKey
     var requestUrlForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=imperial&appid=" + apiKey
-    
+
 
     fetch(requestUrl)
 
@@ -75,17 +75,17 @@ function getApi(event) {
         .then(function (data) {
             // Use the console to examine the response
             console.log(data);
-            
+
             var id = data.weather[0].icon
             console.log(id)
-            var iconUrl = 'https://openweathermap.org/img/w/'+id+'.png'
-            icon.setAttribute("src",iconUrl)
-            var unix = data.dt 
-            var date = Date(unix*1000).slice(0,10)
-            
+            var iconUrl = 'https://openweathermap.org/img/w/' + id + '.png'
+            icon.setAttribute("src", iconUrl)
+            var unix = data.dt
+            var date = Date(unix * 1000).slice(0, 10)
+
             console.log(typeof longDate)
-            place.textContent = city +" "+ date
-            temp.textContent = "Current temperature: " + data.main.temp
+            place.textContent = city + " " + date
+            temp.textContent = "Current temperature: " + data.main.temp+" °F"
             wind.textContent = "Current wind: " + data.wind.speed + "mph"
             humidity.textContent = "Current Humidity: " + data.main.humidity + "%"
             li.textContent = city
@@ -101,7 +101,7 @@ function getApi(event) {
             //   userContainer.append(link)
             // }
         });
-    
+
 
     fetch(requestUrlForecast)
 
@@ -113,47 +113,47 @@ function getApi(event) {
             console.log(data);
 
             var id1 = data.list[6].weather[0].icon
-            var iconUrl1 = 'https://openweathermap.org/img/w/'+id1+'.png'
-            icon1.setAttribute("src",iconUrl1)
+            var iconUrl1 = 'https://openweathermap.org/img/w/' + id1 + '.png'
+            icon1.setAttribute("src", iconUrl1)
 
-            date1.textContent =data.list[6].dt_txt.slice(5,11)
-            temp1.textContent = "Temperature: "+data.list[6].main.temp
-            wind1.textContent = "Wind: "+data.list[6].wind.speed
-            humidity1.textContent = "Humidity: "+data.list[6].main.humidity
+            date1.textContent = data.list[6].dt_txt.slice(5, 11)
+            temp1.textContent = "Temperature: " + data.list[6].main.temp+" °F"
+            wind1.textContent = "Wind: " + data.list[6].wind.speed+ " mph"
+            humidity1.textContent = "Humidity: " + data.list[6].main.humidity+"%"
 
 
             var id2 = data.list[14].weather[0].icon
-            var iconUrl2 = 'https://openweathermap.org/img/w/'+id2+'.png'
-            icon2.setAttribute("src",iconUrl2)
-            date2.textContent = data.list[14].dt_txt.slice(5,11)
-            temp2.textContent = "Temperature: "+data.list[14].main.temp
-            wind2.textContent = "Wind: "+data.list[14].wind.speed
-            humidity2.textContent = "Humidity: "+data.list[14].main.humidity
+            var iconUrl2 = 'https://openweathermap.org/img/w/' + id2 + '.png'
+            icon2.setAttribute("src", iconUrl2)
+            date2.textContent = data.list[14].dt_txt.slice(5, 11)
+            temp2.textContent = "Temperature: " + data.list[14].main.temp+" °F"
+            wind2.textContent = "Wind: " + data.list[14].wind.speed+ " mph"
+            humidity2.textContent = "Humidity: " + data.list[14].main.humidity+"%"
 
             var id3 = data.list[22].weather[0].icon
-            var iconUrl3 = 'https://openweathermap.org/img/w/'+id3+'.png'
-            icon3.setAttribute("src",iconUrl3)
-            date3.textContent =data.list[22].dt_txt.slice(5,11)
-            temp3.textContent = "Temperature: "+data.list[22].main.temp
-            wind3.textContent = "Wind: "+data.list[22].wind.speed
-            humidity3.textContent = "Humidity: "+data.list[22].main.humidity
+            var iconUrl3 = 'https://openweathermap.org/img/w/' + id3 + '.png'
+            icon3.setAttribute("src", iconUrl3)
+            date3.textContent = data.list[22].dt_txt.slice(5, 11)
+            temp3.textContent = "Temperature: " + data.list[22].main.temp+" °F"
+            wind3.textContent = "Wind: " + data.list[22].wind.speed+ " mph"
+            humidity3.textContent = "Humidity: " + data.list[22].main.humidity+"%"
 
             var id4 = data.list[22].weather[0].icon
-            var iconUrl4 = 'https://openweathermap.org/img/w/'+id4+'.png'
-            icon4.setAttribute("src",iconUrl4)
-            date4.textContent =data.list[30].dt_txt.slice(5,11)
-            temp4.textContent = "Temperature: "+data.list[30].main.temp
-            wind4.textContent = "Wind: "+data.list[30].wind.speed
-            humidity4.textContent = "Humidity: "+data.list[30].main.humidity
+            var iconUrl4 = 'https://openweathermap.org/img/w/' + id4 + '.png'
+            icon4.setAttribute("src", iconUrl4)
+            date4.textContent = data.list[30].dt_txt.slice(5, 11)
+            temp4.textContent = "Temperature: " + data.list[30].main.temp+" °F"
+            wind4.textContent = "Wind: " + data.list[30].wind.speed+ " mph"
+            humidity4.textContent = "Humidity: " + data.list[30].main.humidity+"%"
 
 
             var id5 = data.list[38].weather[0].icon
-            var iconUrl5 = 'https://openweathermap.org/img/w/'+id5+'.png'
-            icon5.setAttribute("src",iconUrl5)
-            date5.textContent =data.list[38].dt_txt.slice(5,11)
-            temp5.textContent = "Temperature: "+data.list[38].main.temp
-            wind5.textContent = "Wind: "+data.list[38].wind.speed
-            humidity5.textContent = "Humidity: "+data.list[38].main.humidity
+            var iconUrl5 = 'https://openweathermap.org/img/w/' + id5 + '.png'
+            icon5.setAttribute("src", iconUrl5)
+            date5.textContent = data.list[38].dt_txt.slice(5, 11)
+            temp5.textContent = "Temperature: " + data.list[38].main.temp+" °F"
+            wind5.textContent = "Wind: " + data.list[38].wind.speed+ " mph"
+            humidity5.textContent = "Humidity: " + data.list[38].main.humidity+"%"
         });
 }
 
