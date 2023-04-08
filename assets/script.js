89b62760ef79289d9684d6eb7b123a02b8322c76
Shea Schwenn <sheaschwenn@gmail.com>
2023-04-08 13:48:34 -0700

    main date set, other dates sliced

--- assets/script.js
+++ assets/script.js
@@ -15,6 +15,7 @@
 // api key 
 var searchInput = document.getElementById("search-input")
 var searchButton = document.getElementById("search-btn")
+var place = document.getElementById("place")
 var icon = document.getElementById("icon")
 var temp = document.getElementById("temp");
 var wind = document.getElementById("wind")
@@ -79,6 +80,11 @@ function getApi(event) {
             console.log(id)
             var iconUrl = 'https://openweathermap.org/img/w/'+id+'.png'
             icon.setAttribute("src",iconUrl)
+            var unix = data.dt 
+            var date = Date(unix*1000).slice(0,10)
+            
+            console.log(typeof longDate)
+            place.textContent = city +" "+ date
             temp.textContent = "Current temperature: " + data.main.temp
             wind.textContent = "Current wind: " + data.wind.speed + "mph"
             humidity.textContent = "Current Humidity: " + data.main.humidity + "%"
@@ -110,7 +116,7 @@ function getApi(event) {
             var iconUrl1 = 'https://openweathermap.org/img/w/'+id1+'.png'
             icon1.setAttribute("src",iconUrl1)
 
-            date1.textContent =data.list[6].dt_txt 
+            date1.textContent =data.list[6].dt_txt.slice(5,11)
             temp1.textContent = "Temperature: "+data.list[6].main.temp
             wind1.textContent = "Wind: "+data.list[6].wind.speed
             humidity1.textContent = "Humidity: "+data.list[6].main.humidity
@@ -119,7 +125,7 @@ function getApi(event) {
             var id2 = data.list[14].weather[0].icon
             var iconUrl2 = 'https://openweathermap.org/img/w/'+id2+'.png'
             icon2.setAttribute("src",iconUrl2)
-            date2.textContent =data.list[14].dt_txt 
+            date2.textContent = data.list[14].dt_txt.slice(5,11)
             temp2.textContent = "Temperature: "+data.list[14].main.temp
             wind2.textContent = "Wind: "+data.list[14].wind.speed
             humidity2.textContent = "Humidity: "+data.list[14].main.humidity
@@ -127,7 +133,7 @@ function getApi(event) {
             var id3 = data.list[22].weather[0].icon
             var iconUrl3 = 'https://openweathermap.org/img/w/'+id3+'.png'
             icon3.setAttribute("src",iconUrl3)
-            date3.textContent =data.list[22].dt_txt
+            date3.textContent =data.list[22].dt_txt.slice(5,11)
             temp3.textContent = "Temperature: "+data.list[22].main.temp
             wind3.textContent = "Wind: "+data.list[22].wind.speed
             humidity3.textContent = "Humidity: "+data.list[22].main.humidity
@@ -135,7 +141,7 @@ function getApi(event) {
             var id4 = data.list[22].weather[0].icon
             var iconUrl4 = 'https://openweathermap.org/img/w/'+id4+'.png'
             icon4.setAttribute("src",iconUrl4)
-            date4.textContent =data.list[30].dt_txt
+            date4.textContent =data.list[30].dt_txt.slice(5,11)
             temp4.textContent = "Temperature: "+data.list[30].main.temp
             wind4.textContent = "Wind: "+data.list[30].wind.speed
             humidity4.textContent = "Humidity: "+data.list[30].main.humidity
@@ -144,7 +150,7 @@ function getApi(event) {
             var id5 = data.list[38].weather[0].icon
             var iconUrl5 = 'https://openweathermap.org/img/w/'+id5+'.png'
             icon5.setAttribute("src",iconUrl5)
-            date5.textContent =data.list[38].dt_txt
+            date5.textContent =data.list[38].dt_txt.slice(5,11)
             temp5.textContent = "Temperature: "+data.list[38].main.temp
             wind5.textContent = "Wind: "+data.list[38].wind.speed
             humidity5.textContent = "Humidity: "+data.list[38].main.humidity
